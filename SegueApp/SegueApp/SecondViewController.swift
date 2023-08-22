187157146b7261d27dcd6d687f64f39d4f88ce2e
Nilsu <101411142+nilsusagol@users.noreply.github.com>
2023-08-22 23:15:17 +0300

    Segue App Testing

---
+++ SegueApp/SegueApp/SecondViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SecondViewController.swift
+//  SegueApp
+//
+//  Created by Nilsu Sağol on 29.03.2023.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var namelabel: UILabel!
+    
+    var myName = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        namelabel.text
+    }
+    
+
+    
+}
