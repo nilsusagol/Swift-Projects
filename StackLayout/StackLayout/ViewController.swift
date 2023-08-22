b434fdc9a7d62cd1a0221f978b1937df5f5ae34e
Nilsu <101411142+nilsusagol@users.noreply.github.com>
2023-08-22 23:16:18 +0300

    Stack Layout

---
+++ StackLayout/StackLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StackLayout
+//
+//  Created by Nilsu Sağol on 14.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
